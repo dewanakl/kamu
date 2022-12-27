45b443f5ab978d8d46d5e96ebbd4fc0f2391db70
dewana_kl <53048107+dewanakl@users.noreply.github.com>
2022-12-27 12:24:25 +0700

    feat: update header policy

--- middleware/CorsMiddleware.php
+++ middleware/CorsMiddleware.php
@@ -10,11 +10,12 @@ final class CorsMiddleware implements MiddlewareInterface
 {
     public function handle(Request $request, Closure $next)
     {
-        header('Access-Control-Allow-Origin: *');
         header('X-Content-Type-Options: nosniff');
         header('X-XSS-Protection: 1; mode=block');
         header('X-Frame-Options: SAMEORIGIN');
-        header('Referrer-Policy: origin');
+        header('Referrer-Policy: strict-origin-when-cross-origin');
+        header('Permissions-Policy: geolocation=()');
+        header('Content-Security-Policy: upgrade-insecure-requests');
 
         return $next($request);
     }
