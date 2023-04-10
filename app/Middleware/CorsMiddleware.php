88f77a86cc847702c3f85556ddd6c44117d594f0
dewanakl <dewanakretarta29@gmail.com>
2023-04-10 15:31:07 +0700

    feat: updated cors

--- app/Middleware/CorsMiddleware.php
+++ app/Middleware/CorsMiddleware.php
@@ -11,7 +11,11 @@ final class CorsMiddleware implements MiddlewareInterface
     public function handle(Request $request, Closure $next)
     {
         header('Access-Control-Allow-Origin: ' . baseurl());
-        header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization');
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+        header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, Token');
+        header('Origin: ' . baseurl());
+        header('Vary: Accept-Encoding, Origin');
 
         if ($request->method() != 'OPTIONS') {
             return $next($request);
